c7988146d289901801e4ad695554fc4d68baed8a
Raghuvansh Agarwal <rarchva@gmail.com>
2025-09-30 11:20:01 +0530

    Rendering Ball

--- include/Gameplay/Ball.h
+++ include/Gameplay/Ball.h
@@ -9,10 +9,20 @@
 
 namespace N_Gameplay {
     class Ball {
-        sf::CircleShape ball_sprite_;
-        const float radius_ = 10.0f;
+        sf::Texture texture_;
+        sf::Sprite* sprite_;
+
+        std::string texture_path_ = std::string(PROJECT_ROOT) + "/Assets/Textures/Ball.png";
+
+        const float scale_x = 0.06f;
+        const float scale_y = 0.06f;
+
         const float position_x_ = 615.0f;
         const float position_y_ = 335.0f;
+
+        void loadTexture();
+        void initializeVariables();
+
         public:
         Ball();
         ~Ball();
--- src/Gameplay/Ball.cpp
+++ src/Gameplay/Ball.cpp
@@ -5,18 +5,31 @@
 #include "../../include/Gameplay/Ball.h"
 
 namespace N_Gameplay {
+    void Ball::loadTexture() {
+        if (!texture_.loadFromFile(texture_path_)) {
+            throw std::runtime_error("Failed to load texture from " + texture_path_);
+        }
+    }
+
+    void Ball::initializeVariables() {
+        sprite_ = new sf::Sprite(texture_);
+        sprite_->setScale({scale_x, scale_y});
+        sprite_->setPosition({position_x_, position_y_});
+    }
+
     Ball::Ball() {
-        ball_sprite_.setRadius(radius_);
-        ball_sprite_.setPosition({position_x_, position_y_});
+        loadTexture();
+        initializeVariables();
     }
 
     Ball::~Ball() {
+        delete sprite_;
     }
 
     void Ball::update() {
     }
 
     void Ball::render(sf::RenderWindow *p_window) {
-        p_window->draw(ball_sprite_);
+        p_window->draw(*sprite_);
     }
 } // N_Gameplay
